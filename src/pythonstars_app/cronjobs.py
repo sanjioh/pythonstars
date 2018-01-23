8790576e818debb89377cbb841622c7204b3c087
Fabio Sangiovanni <sjh+github@sanjioh.org>
2018-01-23 16:03:11 +0100

    get new data every 6 hours

--- src/pythonstars_app/cronjobs.py
+++ src/pythonstars_app/cronjobs.py
@@ -6,7 +6,7 @@ from pythonstars_app.models import DataPoint
 
 
 class GetDataCronjob(CronJobBase):
-    RUN_EVERY_MINS = 30  # every 30 minutes
+    RUN_EVERY_MINS = 360  # 6 hours
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'pythonstars_app.cronjobs.GetDataCronjob'
